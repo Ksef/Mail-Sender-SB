5279b2dc8ebd0b2dfa1d83a2d111deec2222144d
GrayHead <ss.zhuravlov@gmail.com>
2021-08-28 21:25:42 +0300

    lesson 5 list set

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -2,38 +2,127 @@ package com.example.demo.controllers;
 
 import com.example.demo.dao.UserDAO;
 import com.example.demo.models.User;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:4200"})
+@AllArgsConstructor
 public class MainController {
+
 
     private UserDAO userDAO;
 
-    public MainController(UserDAO userDAO) {
-        this.userDAO = userDAO;
-    }
 
     @GetMapping("/users")
     public List<User> users() {
-        List<User> all = userDAO.findAll();
-        return all;
+        List<User> users = userDAO.findAll();
+//        ArrayList<User> arrayList = new ArrayList<>(users);
+//        arrayList.add()
+//        arrayList.get()
+//        arrayList.remove()
+//        arrayList.set(12, new User());
+//        arrayList.contains(new User());
+//        arrayList.containsAll(collection);
+
+//        LinkedList<User> linkedList = new LinkedList<>(users);
+//        linkedList.add()
+//        linkedList.get()
+//        linkedList.remove()
+//        linkedList.set()
+//        linkedList.contains()
+//        linkedList.containsAll(collection);
+
+//        linkedList.addFirst(new User());
+//        linkedList.addLast(new User());
+//        linkedList.removeFirst();
+//        linkedList.removeLast();
+//        linkedList.removeFirstOccurrence(new User());
+
+        /*----set----*/
+
+        System.out.println(users);
+
+//        Set<User> userSet = new HashSet<>(users);
+//        System.out.println(userSet);
+//        for (User user : userSet) {
+//            System.out.println(user.hashCode());
+//        }
+
+//        HashSet<User> hashSet = new HashSet<>();
+//        hashSet.add(new User(2, "kokos"));
+////        hashSet.add(new User(2, "kokos"));
+//        hashSet.add(new User(1, "ananas"));
+////        hashSet.add(new User(1, "ananas"));
+//        hashSet.add(new User(3, "banan"));
+//        hashSet.add(new User(5, "tomat"));
+//        hashSet.add(new User(4, "potatos"));
+////        hashSet.add(new User(4, "potatos"));
+//        hashSet.add(new User(6, "mango"));
+//        System.out.println(hashSet);
+//        Iterator<User> iterator = hashSet.iterator();
+//        while (iterator.hasNext()) {
+//            User next = iterator.next();
+//            if (next.getId() % 2 == 0) {
+//                iterator.remove();
+//            }
+//
+//        }
+//        System.out.println(hashSet);
+
+
+//        Set<User> linkedHashSet = new LinkedHashSet<>();
+//        linkedHashSet.add(new User(2, "kokos"));
+//        linkedHashSet.add(new User(2, "kokos"));
+//        linkedHashSet.add(new User(1, "ananas"));
+//        linkedHashSet.add(new User(1, "ananas"));
+//        linkedHashSet.add(new User(3, "banan"));
+//        linkedHashSet.add(new User(5, "tomat"));
+//        linkedHashSet.add(new User(4, "potatos"));
+//        linkedHashSet.add(new User(4, "potatos"));
+//        linkedHashSet.add(new User(6, "mango"));
+//
+
+
+//        TreeSet<User> treeSet = new TreeSet<>();
+//        treeSet.add(new User(2, "kokos"));
+//        treeSet.add(new User(1, "ananas"));
+//        treeSet.add(new User(3, "banan"));
+//        treeSet.add(new User(5, "tomat"));
+//        treeSet.add(new User(4, "potatos"));
+//        treeSet.add(new User(6, "mango"));
+//        System.out.println(treeSet);
+
+        return users;
     }
 
-    @PostMapping("/user")
-    public void saveUser(@RequestBody User user) {
-        System.out.println(user);
+    @PostMapping("/users")
+    public User saveUser(@RequestBody User user) {
         userDAO.save(user);
+        return user;
+
 
     }
 
     @GetMapping("/users/{id}")
     public User getUser(@PathVariable int id) {
-        User user = userDAO.findById(id).get();
-        return user;
+        return userDAO.findById(id).get();
     }
+
+    @PatchMapping("/users")
+    public User updateUser(@RequestBody User userFromrequest) {
+        User userFromDb = userDAO.getById(userFromrequest.getId());
+        userDAO.save(userFromrequest);
+        return userFromrequest;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        userDAO.deleteById(id);
+
+    }
+
 }
 
--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,54 +1,36 @@
 package com.example.demo.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.*;
 
+import javax.persistence.*;
+
+@Table(name = "User")
 @Entity
-public class User {
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+
+public class User implements Comparable<User> {
+    @Column(nullable = false)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private String username;
 
-    public User() {
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public User(String name) {
         this.name = name;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+    public int compareTo(User o) {
+//        System.out.println(this);
+        return this.getId() - o.getId();
+//        return this.getName().compareTo(o.getName());
     }
 }
